b893fb4f07760cce51defe3e5668d354e8c60a0d
Anna Szpoton <anna.szpoton@cloudify.co>
2022-05-24 10:46:02 +0200

    Init

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.name
+  location = var.location
+  tags     = var.tags
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "name" {
+
+}
+
+variable "location" {
+
+}
+
+variable "tags" {
+  type    = "map"
+  default = {}
+}
+
+variable "role_name" {
+  default = ""
+}
